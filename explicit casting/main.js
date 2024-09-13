769faf66cf83db353e8e4efd8a5f88c4e10133d1
Fariha zia <161895273+FarihaZia@users.noreply.github.com>
2024-09-13 16:01:52 +0500

    Delete explicit casting directory

--- explicit casting/main.js
+++
@@ -1,9 +0,0 @@
-var value = "Ali";
-console.log(value.toUpperCase() // here we assigned the type
-);
-var x = 'hello';
-console.log(x.length);
-var val = 3;
-console.log(val);
-var val1 = 4;
-console.log(val1.length);
